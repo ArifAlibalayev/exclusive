ee7a60c2a1ea55bcce3ed6b3b1da964c6bb3ecd0
ArifAlibalayev <mi7h4r3f8@code.edu.az>
2023-12-20 19:42:08 +0400

    20.12

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import OurStory from '../../components/OurStory'
+import AboutCategories from '../../components/AboutCategories'
 
 function AboutPage() {
   return (
     <>
     <OurStory/>
+    <AboutCategories/>
     </>
   )
 }
